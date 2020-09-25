89067ea977c6427258a7ead1e328095c613506a5
Gabriel Mota <gabrielfmota134@gmail.com>
2020-09-25 19:43:08 -0300

    Add get/opportunities
    Signed-off-by: Gabriel Mota <gabrielfmota134@gmail.com>

--- src/index.js
+++ src/index.js
@@ -4,9 +4,24 @@ const bodyParser = require('body-parser')
 const app = express()
 
 app.use(bodyParser())
-
-app.get('/', (_req, res) => {
-  res.send('OK, sucesso!')
+const allOpportunities = [
+  {
+    id: 1,
+    title: "IC em Desenvolvimento de Sistemas",
+    author: "João Daniel",
+    description: "Desenvolvimento de um sistema na arquitetura de microsserviços para estudar os desdobramentos relativos dos padrões"
+  },
+  {
+    id: 2,
+    title: "Estágio de desenvolvimento em BluBank",
+    author: "BluBank",
+    description: "Estágio 20h/semana, benefícios VR+Odonto, bolsa-auxílio compatível com mercado"
+  }
+]
+app.get('/opportunities', (_req, res) => {
+  res.json({
+    opportunities: allOpportunities
+  })
 })
 
 app.listen(3000, () => {

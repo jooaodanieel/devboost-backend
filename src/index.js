2b02f7f73cecd5780953693ab681e85c7eb46d10
Thiago Guerrero <tgbalera@hotmail.com>
2020-09-29 20:23:38 -0300

    Refactoring post validation to opportunities
    Signed-off-by: Thiago Guerrero <tgbalera@hotmail.com>

--- src/index.js
+++ src/index.js
@@ -5,58 +5,67 @@ const app = express()
 
 app.use(bodyParser())
 
-let id = 1;
+function validate(title, author, description) {
+  const errors = []
+  if (title === undefined || title.trim() === '') {
+    errors.push('Empty title')
+  }
+
+  if (author === undefined || author.trim() === '') {
+    errors.push('Empty author')
+  }
+
+  if (description === undefined || description.trim() === '') {
+    errors.push('Empty description')
+  }
+  return {
+    isValid: errors.length === 0,
+    errors,
+  }
+}
 
 const allOpportunities = [
   {
     id: 1,
-    title: "IC em Desenvolvimento de Sistemas",
-    author: "João Daniel",
-    description: "Desenvolvimento de um sistema na arquitetura de microsserviços para estudar os desdobramentos relativos dos padrões"
+    title: 'IC em Desenvolvimento de Sistemas',
+    author: 'João Daniel',
+    description:
+      'Desenvolvimento de um sistema na arquitetura de microsserviços para estudar os desdobramentos relativos dos padrões',
   },
   {
     id: 2,
-    title: "Estágio de desenvolvimento em BluBank",
-    author: "BluBank",
-    description: "Estágio 20h/semana, benefícios VR+Odonto, bolsa-auxílio compatível com mercado"
-  }
+    title: 'Estágio de desenvolvimento em BluBank',
+    author: 'BluBank',
+    description:
+      'Estágio 20h/semana, benefícios VR+Odonto, bolsa-auxílio compatível com mercado',
+  },
 ]
 app.get('/opportunities', (_req, res) => {
   res.json({
-    opportunities: allOpportunities
+    opportunities: allOpportunities,
   })
 })
 
 app.post('/opportunities', (req, res) => {
-    const { title, author, description } = req.body;
+  const { title, author, description } = req.body
+  const { isValid, errors } = validate(title, author, description)
 
-    if (title === undefined || title.trim() === "") {
-        res.status(400).send("error: empty title");
-        return;
-    }
+  if (!isValid) {
+    res.status(400).json(errors)
+    return
+  }
 
-    if (author === undefined || author.trim() === "") {
-        res.status(400).send("error: empty author");
-        return;
-    }
+  const opportunity = {
+    id: allOpportunities.length + 1,
+    title,
+    author,
+    description,
+  }
 
-    if (description === undefined || description.trim() === "") {
-        res.status(400).send("error: empty description");
-        return;
-    }
-
-    const opportunity = {
-        id,
-        title,
-        author,
-        description
-    }
-
-    id++;
-
-    console.log(opportunity);
-    res.send(opportunity);
-});
+  allOpportunities.push(opportunity)
+  console.log(opportunity)
+  res.json(opportunity)
+})
 
 app.listen(3000, () => {
   console.log('server running')

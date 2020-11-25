47e7e792998ae2822341e699674a7270aa5012cc
Francisco Eugênio Wernke <franwernke@usp.com>
2020-11-24 21:07:48 -0300

    WIP: Refactor filter and create one field search option
    Signed-off-by: Édio Cerati Neto <edioceratineto@gmail.com>
    Signed-off-by: Francisco Eugênio Wernke <franwernke@usp.br>
    Signed-off-by: Wander da Souza <wanderdasouza@gmail.com>
    Signed-off-by: Gabriel Mota <gabrielfmota134@gmail.com>
    Signed-off-by: erickrodrigs <erick.rodrigues.santana14@gmail.com>

--- src/index.js
+++ src/index.js
@@ -10,36 +10,66 @@ app.use(cors())
 function validate(title, author, description, summary, tags) {
   const errors = []
   if (title === undefined || title.trim() === '') {
-    errors.push('Empty title');
+    errors.push('Empty title')
   }
 
   if (author === undefined || author.trim() === '') {
-    errors.push('Empty author');
+    errors.push('Empty author')
   }
 
   if (description === undefined || description.trim() === '') {
-    errors.push('Empty description');
+    errors.push('Empty description')
   }
 
   if (summary === undefined || summary.trim() === '') {
-    errors.push('Empty summary');
+    errors.push('Empty summary')
   }
 
-  if (!Array.isArray(tags)){
-    errors.push("Tags must be an array");
-  }
-  else{
+  if (!Array.isArray(tags)) {
+    errors.push('Tags must be an array')
+  } else {
     const isString = tags.every((el) => {
-      return typeof(el) === "string";
+      return typeof el === 'string'
     })
-    if (!isString)
-      errors.push("All tags must be strings");
+    if (!isString) errors.push('All tags must be strings')
   }
 
   return {
     isValid: errors.length === 0,
     errors,
   }
+}
+
+function within(object, key) {
+  return object.toLowerCase().includes(key.toLowerCase())
+}
+
+function filter({
+  queryAuthorValid,
+  queryDescriptionValid,
+  queryTitleValid,
+  data,
+}) {
+  const { general, title, author, description } = data
+
+  return allOpportunities.filter((opportunity) => {
+    if (queryAuthorValid || queryDescriptionValid || queryTitleValid) {
+      return (
+        (queryTitleValid && within(opportunity.title, title)) ||
+        (queryAuthorValid && within(opportunity.author, author)) ||
+        (queryDescriptionValid && within(opportunity.description, description))
+      )
+    } else {
+      return Object.keys(opportunity).some((key) => {
+        if (key == 'image' || key == 'id') {
+          return false
+        } else if (key == 'tags') {
+          return opportunity[key].some((item) => within(item, general))
+        }
+        return within(opportunity[key], general)
+      })
+    }
+  })
 }
 
 const allOpportunities = [
@@ -50,8 +80,8 @@ const allOpportunities = [
     summary: '',
     description:
       'Desenvolvimento de um sistema na arquitetura de microsserviços para estudar os desdobramentos relativos dos padrões',
-    tags: ["Iniciação Científica", "Bolsa", "FAPESP", "Sistemas"],
-    },
+    tags: ['Iniciação Científica', 'Bolsa', 'FAPESP', 'Sistemas'],
+  },
   {
     id: 2,
     title: 'Estágio de desenvolvimento em BluBank',
@@ -59,7 +89,7 @@ const allOpportunities = [
     summary: '',
     description:
       'Estágio 20h/semana, benefícios VR+Odonto, bolsa-auxílio compatível com mercado',
-    tags: ["Estágio", "Sistemas"],
+    tags: ['Estágio', 'Sistemas'],
   },
 ]
 
@@ -67,41 +97,40 @@ app.get('/opportunities/tags', (_req, res) => {
   const tags = allOpportunities.reduce((acc, opp) => {
     opp.tags.forEach((tag) => {
       if (!acc[tag]) {
-        acc[tag] = tag;
+        acc[tag] = tag
       }
     })
-    return acc;
+    return acc
   }, {})
 
   res.json({
-    tags: Object.keys(tags)
+    tags: Object.keys(tags),
   })
 })
 
 app.get('/opportunities', (_req, res) => {
-  const { title, author, description } = _req.query
-  const queryTitleValid = title != undefined && title.trim() != ''
-  const queryAuthorValid = author != undefined && author.trim() != ''
-  const queryDescriptionValid =
-    description != undefined && description.trim() != ''
+  const { general, title, author, description } = _req.query
+  const queryGeneralValid = general != undefined && general.trim() != ''
+
+  const query = {
+    queryAuthorValid: author != undefined && author.trim() != '',
+    queryDescriptionValid: description != undefined && description.trim() != '',
+    queryTitleValid: title != undefined && title.trim() != '',
+    data: {
+      general,
+      title,
+      author,
+      description,
+    },
+  }
 
   const hasQueries =
-    queryAuthorValid || queryDescriptionValid || queryTitleValid
+    queryGeneralValid ||
+    query.queryAuthorValid ||
+    query.queryDescriptionValid ||
+    query.queryTitleValid
 
-  const filtered = hasQueries
-    ? allOpportunities.filter((opportunity) => {
-        return (
-          (queryTitleValid &&
-            opportunity.title.toLowerCase().includes(title.toLowerCase())) ||
-          (queryAuthorValid &&
-            opportunity.author.toLowerCase().includes(author.toLowerCase())) ||
-          (queryDescriptionValid &&
-            opportunity.description
-              .toLowerCase()
-              .includes(description.toLowerCase()))
-        )
-      })
-    : allOpportunities
+  const filtered = hasQueries ? filter(query) : allOpportunities
 
   res.json({
     opportunities: filtered,
@@ -110,11 +139,17 @@ app.get('/opportunities', (_req, res) => {
 
 app.post('/opportunities', (req, res) => {
   const { title, author, description, summary, tags } = req.body
-  const { isValid, errors } = validate(title, author, description, summary, tags)
+  const { isValid, errors } = validate(
+    title,
+    author,
+    description,
+    summary,
+    tags
+  )
 
   if (!isValid) {
-    console.log(errors);
-    res.json({ status: 400, message: errors });
+    console.log(errors)
+    res.json({ status: 400, message: errors })
     return
   }
 
@@ -124,7 +159,7 @@ app.post('/opportunities', (req, res) => {
     author,
     description,
     summary,
-    tags
+    tags,
   }
 
   allOpportunities.push(opportunity)
